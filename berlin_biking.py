9c1d6024acd0c8dbdebcfdc83a6ddaf8dd4c295d
Jan Balke <jan@pukao.de>
2016-10-26 11:52:53 +0200

    Initial import of the script
    It parses the rss feed and searches for defined terms in the rss
    article link.
    If a match was found it prints it to the console.

---
+++ berlin_biking.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+##
+## Fetch the police rss feed and look for bike incidents.
+## Follow the link and look behind it if it is biking related
+##
+## Database schema
+##
+## | ID | DATE | LOCATION | TITLE | LINK | TEXT |
+
+
+import json
+import feedparser
+import requests
+from lxml import html
+
+"""
+Configuration
+"""
+# police feed
+feed_url = "https://www.berlin.de/polizei/polizeimeldungen/index.php/rss"
+# search terms
+whitelist = ["Fahrrad", "Radfahrer", "Radfahrerin", "Fahrradfahrer", "Fahrradfahrerin", "E-Bike", "Radweg"]
+# cookie path
+cookie_path = ".berlin_biking.cookie"
+
+
+def read_cookie():
+  try:
+    with open(cookie_path, "r") as f:
+        return json.load(f)
+  except (Exception):
+    pass
+
+  return {"last_links": []}
+
+
+def write_cookie(cookie):
+  try:
+    with open(cookie_path, "w") as f:
+      json.dump(cookie, f)
+  except Exception as e:
+    print(e)
+
+
+
+def check_item(item):
+  # look into the content
+  content = requests.get(item['link'])
+  tree = html.fromstring(content.content)
+
+  # the date
+  d = "None"
+  for polizeimeldung in tree.xpath('//div[@role = "main"]//div[@class = "polizeimeldung"]/text()'):
+    if "Polizeimeldung vom" in polizeimeldung:
+      # it is a date
+      d = polizeimeldung.split()[-1]
+  # the text
+  c = tree.xpath('//div[@role = "main"]//div[@class = "textile"]/p/text()')
+  text = "".join(c)
+
+  tokens = text.lower().split()
+  if any( term.lower() in tokens for term in whitelist):
+    print("Another Bike incident:")
+    print(" Title: '{}'.".format(item['title']))
+    print(" Date: '{}'".format(d))
+    print(" Link: '{}'".format(item['link']))
+    print("")
+
+
+
+
+
+content = feedparser.parse(feed_url)
+if not content['feed']:
+  print("Failed to fetch rss feed")
+else:
+  items = content['entries']
+  print("Fetched {} items".format(len(items)))
+
+  new_links = []
+  cookie = read_cookie()
+
+  print("New reports:")
+  for item in items:
+    new_links.append(item['link'])
+    if not new_links[-1] in cookie['last_links']:
+      check_item(item)
+
+  cookie['last_links'] = new_links
+  write_cookie(cookie)
+
+print("\nDone")
